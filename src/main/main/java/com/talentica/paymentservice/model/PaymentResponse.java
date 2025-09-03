2acb84fb02b675c1b8eae1f00aad906d909669c8
ramandeep-kumar <ramandeep.kumar@talentica.com>
2025-09-03 16:05:12 +0530

    Create PaymentResponse.java

---
+++ src/main/main/java/com/talentica/paymentservice/model/PaymentResponse.java
@@ -0,0 +1,9 @@
+package com.talentica.paymentservice.model;
+
+public class PaymentResponse {
+    private String status;
+    private String transactionId;
+    private String message;
+
+    // Getters and Setters
+}

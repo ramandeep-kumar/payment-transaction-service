0f3e2171f90e3bd2c97637b95ba90087f3367400
ramandeep-kumar <ramandeep.kumar@talentica.com>
2025-09-03 16:04:53 +0530

    Create PaymentRequest.java

---
+++ src/main/main/java/com/talentica/paymentservice/model/PaymentRequest.java
@@ -0,0 +1,9 @@
+package com.talentica.paymentservice.model;
+
+public class PaymentRequest {
+    private Double amount;
+    private String currency;
+    private String paymentMethodNonce; // For card info or tokenization
+
+    // Getters and Setters
+}
